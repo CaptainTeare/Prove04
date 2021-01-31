c3b9b7e70b05eb5eb82002c7570590d72e07f607
Brody Roelofs <u771368366@byui.edu>
2021-01-30 20:38:00 -0700

    I did what i needed to do

--- src/teare/Main.java
+++ src/teare/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	  System.out.print("Hello my name is Brayton. Yee.");
+	  System.out.print("Hello my name is Brayton. Yee.");
     }
 }
